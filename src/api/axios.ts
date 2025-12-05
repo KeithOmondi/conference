360ce6fec42e104c356de24bcea80539f3aa9019
KeithOmondi <denniskeith62@gmail.com>
2025-12-05 15:50:18 +0300

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-baseURL: "http://localhost:5000/api/v1",
+baseURL: "https://conference1.onrender.com/api/v1",
 withCredentials: true, // include cookies automatically
 headers: {
 "Content-Type": "application/json",
